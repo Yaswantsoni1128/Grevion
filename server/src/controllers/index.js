2dfe3b00ead1e873cd9e6333f3211a13c767b135
Srishti <srishtisethi31@gmail.com>
2025-03-31 22:43:03 +0530

    declineRequest added

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -3,9 +3,9 @@ import { sendOtp,login,signUp } from "./Auth.controllers.js";
 import { completeProfile , getUserProfile } from "./CompleteProfile.controllers.js";
 
 import { getAllSpoc , placeOrder, getAllOrders} from "./PowerPlant.controllers.js";
-import { addFarmer, updateFarmer, getAllFarmers ,deleteFarmer,getAllRequests, acceptRequest} from "./Spoc.controllers.js";
+import { addFarmer, updateFarmer, getAllFarmers ,deleteFarmer,getAllRequests, acceptRequest, declineRequest} from "./Spoc.controllers.js";
 
-export { sendOtp,login,signUp, completeProfile , getUserProfile, addFarmer, updateFarmer, getAllFarmers, deleteFarmer, getAllSpoc, placeOrder,getAllRequests ,acceptRequest,getAllOrders}
+export { sendOtp,login,signUp, completeProfile , getUserProfile, addFarmer, updateFarmer, getAllFarmers, deleteFarmer, getAllSpoc, placeOrder,getAllRequests ,acceptRequest,getAllOrders, declineRequest}
 
 
 

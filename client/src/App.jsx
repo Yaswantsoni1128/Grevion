66bba0d7295f7576be38a855300f7f5a7e97c232
Yaswantsoni1128 <yaswantsoni2005@gmail.com>
2025-04-01 19:57:24 +0530

    protected route setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer.jsx';
-import { Route, Routes } from 'react-router-dom';
 import { LoginContextProvider } from './context';
 
 import LandingPage from './pages/LandingPage/LandingPage.jsx';
@@ -11,8 +11,6 @@ import WhoWeAre from './pages/LandingPage/WhoWeAre.jsx';
 
 import Login from './pages/Login/Login.jsx';
 import Signup from './pages/Signup/Signup.jsx';
-
-import Dashboard from './pages/Dashboard.jsx';
 
 import Spoc_dashboard from './pages/spoc/Spoc_dashboard.jsx';
 import AddFarmer from './pages/spoc/AddFarmerPage.jsx';
@@ -24,39 +22,38 @@ import PowerplantDashboard from './pages/powerplant/PowerplantDashboard.jsx';
 import MakePayment from './pages/powerplant/MakePaymentPage.jsx';
 import MyOrdersPage from './pages/powerplant/MyOrdersPage.jsx';
 import SpocsListing from './pages/powerplant/SpocsListingPage.jsx';
+import ProtectedRoute from './components/ProtectedRoute.jsx';
 
 const App = () => {
   return (
     <LoginContextProvider value={{}}>
       <Navbar />
       <Routes>
-        
-          <Route path='/' element={<LandingPage />} />
-          <Route path='/landingPage' element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/landingPage" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
 
-          {/* Landing Page Routes */}
-          <Route path="/landingPage/sustainability" element={<Sustainability />} />
-          <Route path="/landingPage/whatwedo" element={<WhatWeDo />} />
-          <Route path="/landingPage/whoweare" element={<WhoWeAre />} />
+        {/* Landing Page Routes */}
+        <Route path="/landingPage/sustainability" element={<Sustainability />} />
+        <Route path="/landingPage/whatwedo" element={<WhatWeDo />} />
+        <Route path="/landingPage/whoweare" element={<WhoWeAre />} />
 
-          {/* Dashboard */}
-          <Route path="/dashboard" element={<Dashboard />} />
+        {/* Protected Routes */}
+        <Route path="/spoc/*" element={<ProtectedRoute />}>
+          <Route path="dashboard" element={<Spoc_dashboard />} />
+          <Route path="add-farmer" element={<AddFarmer />} />
+          <Route path="farmer-listing" element={<FarmerListing />} />
+          <Route path="paralis-request" element={<ParalisRequest />} />
+          <Route path="profile" element={<SpocProfile />} />
+        </Route>
 
-          {/* SPOC Routes */}
-          <Route path="/spoc/dashboard" element={<Spoc_dashboard />} />
-          <Route path="/spoc/add-farmer" element={<AddFarmer />} />
-          <Route path="/spoc/farmer-listing" element={<FarmerListing />} />
-          <Route path="/spoc/paralis-request" element={<ParalisRequest />} />
-          <Route path="/spoc/profile" element={<SpocProfile />} />
-
-          {/* Powerplant Routes */}
-          <Route path="/powerplant/dashboard" element={<PowerplantDashboard />} />
-          <Route path="/powerplant/make-payment" element={<MakePayment />} />
-          <Route path="/powerplant/my-orders" element={<MyOrdersPage />} />
-          <Route path="/powerplant/spocs-listing" element={<SpocsListing />} />
-        
+        <Route path="/powerplant/*" element={<ProtectedRoute />}>
+          <Route path="dashboard" element={<PowerplantDashboard />} />
+          <Route path="make-payment" element={<MakePayment />} />
+          <Route path="my-orders" element={<MyOrdersPage />} />
+          <Route path="spocs-listing" element={<SpocsListing />} />
+        </Route>
       </Routes>
       <Footer />
     </LoginContextProvider>
